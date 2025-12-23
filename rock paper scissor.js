5a94b9f0074fcef83ef4366ff714f14ff0bf3242
QinQipeng <126496939+QinQipeng@users.noreply.github.com>
2025-12-23 16:05:48 +0800

    Completed Revisiting Rock Paper Scissor
    A simple interactive UI is added

--- rock paper scissor.js
+++ rock paper scissor.js
@@ -12,34 +12,70 @@ function getComputerChoice () {
     }
 }
 
-function getHumanChoice () {
-    return prompt("Enter either rock, paper, or scissor").toLowerCase()
-}
-
-function playRound (humanChoice, computerChoice) {
+function playRound (humanChoice, computerChoice, humanScore, computerScore) {
+    let message = "";
+    let winner = "";
     if (humanChoice == computerChoice){
-        console.log(`Draw! ${humanChoice} against ${computerChoice}`)
+        winner = "tie";
     }else if (humanChoice == "rock"){
-        if(computerChoice == "scissor") {console.log("You win! Rock beats Scissor");   humanScore++}
-        else                            {console.log("You lose! Paper beats Rock");    computerScore++}
+        winner = computerChoice == "scissor" ? "human" : "computer";
     } else if (humanChoice == "paper"){
-        if(computerChoice == "rock")    {console.log("You win! Paper beats Rock");     humanScore++;}
-        else                            {console.log("You lose! Scissor beats Paper"); computerScore++}
+        winner = computerChoice == "rock" ? "human" : "computer";
     } else {
-        if (computerChoice == "paper")  {console.log("You Win! Scissor beats Paper");  humanScore++}
-        else                            {console.log("You lose! Rock beats Scissor");  computerScore++}
+        winner = computerChoice == "paper" ? "human" : "computer";
     }
+    if (winner == "human"){
+        message = `You scores! ${humanChoice} beats ${computerChoice}!`;
+        humanScore.textContent = parseInt(humanScore.textContent) + 1;
+    }else if (winner == "computer"){
+        message = `Computer scores! ${computerChoice} beats ${humanChoice}!`;
+        computerScore.textContent = parseInt(computerScore.textContent) + 1;
+    }else {
+        message = `It's a tie! You both chose ${humanChoice}!`;
+    }
+    return message;
 }
 
-let humanScore = 0
-let computerScore = 0
-
-for (let i = 0 ; i < 5; i++){
-    const humanSelection = getHumanChoice()
-    const computerSelection = getComputerChoice()
-
-    playRound(humanSelection, computerSelection)
+function resetGame(humanScore, computerScore, humanLabel, computerLabel, messageLabel) {
+    humanScore.textContent = "0";
+    computerScore.textContent = "0";
+    humanLabel.textContent = "None";
+    computerLabel.textContent = "None";
+    messageLabel.textContent = "";
 }
 
-console.log(`Human score: ${humanScore} vs Computer score: ${computerScore}`)
-console.log(`${humanScore > computerScore ? "Human" : "Computer"} wins!`)
+
+const humanScore = document.querySelector("#human-score");
+const computerScore = document.querySelector("#computer-score");
+const humanLabel = document.querySelector("#human-choice");
+const computerLabel = document.querySelector("#computer-choice");
+const messageLabel = document.querySelector("#message");
+
+const choice = document.querySelector("#choices");
+choice.addEventListener("click", (event) => {
+
+    let humanChoice = event.target.id;
+    let computerChoice = getComputerChoice();
+    const result = playRound(humanChoice, computerChoice, humanScore, computerScore);
+
+    messageLabel.textContent = result;
+    humanLabel.textContent = `${humanChoice}`;
+    computerLabel.textContent = `${computerChoice}`;
+
+    if (parseInt(humanScore.textContent) >= 5 || parseInt(computerScore.textContent) >= 5){
+        let game_over_event = new CustomEvent("game_over");
+        document.dispatchEvent(game_over_event);
+    }
+});
+
+document.addEventListener("game_over", () => {
+    setTimeout(() => {
+        if (parseInt(humanScore.textContent) >= 5){
+            alert("Congratulations! You won the game! (Click OK to reset scores)");
+            resetGame(humanScore, computerScore, humanLabel, computerLabel, messageLabel);
+        }else if (parseInt(computerScore.textContent) >= 5){
+            alert("Computer won the game, try again! (Click OK to reset scores)");
+            resetGame(humanScore, computerScore, humanLabel, computerLabel, messageLabel);
+        }
+    }, 100); // Just to ensure the last score update is visible before alert
+});
